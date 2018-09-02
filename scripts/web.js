6f7a75c15ae8aa0d37efaa1efbcd158ee4d42c10
Fred Siika <fsiika@e1z1r4p18.42.us.org>
2018-09-01 18:05:45 -0700

    Began writing websever and build config.

---
+++ scripts/web.js
@@ -0,0 +1,78 @@
+import { fstat } from "fs";
+
+/* This is a a web builder script that generates the web files for this repository 
+*
+*
+*
+*
+*/
+ 
+// Load module
+
+
+// Travis CI config files
+
+
+// Compile mini.css frameworks and custom styles, ising node-sass
+
+
+// Set variables for the $PATH
+
+
+// Start the timer for the script
+
+
+// Load static parts of this repo (snippets/glossar/etc)
+
+
+// Load tag data from datatbase in (./glossary/keyword_database)
+
+
+// Shuffle the array of snippets
+
+
+// Generate the HTML for selected snippets
+
+
+// Optimize operator nodes
+
+
+// Pull the daily picks into the HTML webpage
+
+
+// Send requests and wait for the response, write to the pag
+
+
+
+// Generate the `index.html` files
+
+
+// Create the output for individual category pages
+
+
+// Minify resently rendered output from previous category pages
+pagesOuput.forEach(page () => {
+    page.content = minify(page.content, {
+        collapseBooleanAttributes: true,
+        collapseWhiteSpace: true,
+        decodeEntities: false,
+        minifyJS: true,
+        keepClosingSlash: true,
+        processConditionalComments: true,
+        removeAttributeQuotes: false,
+        removeComments: true,
+        removeEmpyAttribtues: true, 
+        removeScriptTypeAttributes: false,
+        removeStyleLinkTypeAttributes: false,
+        trimCustomFragments: true;
+    });
+    fs.writeFilesSync(paths.join(docsPath, page.tag + '.html'), + page.content);
+});
+
+// Create the output for the `beginner.html` page
+try {
+    // Add the static parts
+    beginning output += `${beginningStartPart + '\n'}`;
+}
+
+filter beginning snippets
