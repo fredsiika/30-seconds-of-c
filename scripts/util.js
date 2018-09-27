94527ae7ce4c5130e1d3a7d022ebd020587c75c0
Fred Siika <frsiika@e1z1r4p37.42.us.org>
2018-09-27 07:52:53 -0700

    Created demo utility lib that gets list of snippets

---
+++ scripts/util.js
@@ -0,0 +1,28 @@
+/*
+ *  Title: Snippets Library (Demo)
+ *  Description: Utility library for getting a list of snippets.
+ *  Author: Fred C. Siika
+ *  Date: 27 September 2018
+ */
+
+//  Dependencies
+const fs = require('fs');
+
+//  App object
+const snippets = {};
+
+// Get all the snippets and return them to the user
+snippets.allSnippets = function(){
+    
+    // Read the text file containing the snippets
+    let fileContents = fs.readFileSync(__dirname + '/snippets.txt', 'utf8');
+
+    // Turn the string into an array
+    let arrayOfSnippets = fileContents.split(/\r?\n/);
+
+    // Return the array
+    return arrayOfSnippets
+}
+
+// Export the library
+module.exports = snippets;
