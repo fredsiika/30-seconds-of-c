67450bb9f110793318719183a7b8627b09a59ab7
Fred Siika <fredsiika@gmail.com>
2018-10-07 05:35:00 -0700

    Added ft_is_negative

--- snippets/ft_is_negative/ft_is_negative.c
+++ snippets/ft_is_negative/ft_is_negative.c
@@ -6,34 +6,39 @@
 /*   By: frsiika <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/07 04:51:13 by frsiika           #+#    #+#             */
-/*   Updated: 2018/10/07 05:00:06 by frsiika          ###   ########.fr       */
+/*   Updated: 2018/10/07 05:30:38 by frsiika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
+	
+void    ft_putchar(char c)
 {
 		write(1, &c, 1);
 }
-
-void	ft_putstr(char *str)
-{
-		ft_putchar(*str);
-			str++;
+	
+void    ft_putstr(char *str)
+{ 
+	  	while (*str != '\0')
+	    	ft_putstr("Is the number positive or negative?"); 
+	      *str++;
 }
-
-void	ft_is_negative(int n)
-{
-	if (n >= 0)
-		ft_putstr("Positive");
-	else
-		ft_putstr("Negative");
+	
+void    ft_is_negative(int n)
+{  
+	    if (n >= 0)
+	        ft_putchar('P');
+    	else if (n < 0)
+			ft_putchar('N');
 }
-
-int main()
+int		main()
 {
-	ft_putstr("Is the number positive or negative?");
-	ft_is_negative();
-	return (0);
+		int n;
+
+		ft_is_negative(40);
+		ft_is_negative(-50);
+		ft_is_negative(234324);
+		ft_is_negative(-1);
+		ft_is_negative(-5);
+		return (0);
 }
--- snippets/ft_is_negative/ft_is_negative.md
+++ snippets/ft_is_negative/ft_is_negative.md
@@ -1,35 +1,41 @@
 # ft_is_negative
+This function declares n as integer, takes in a numberical value and prints outputs if the value is 'P' for positive or 'N' for negative.
 
 ```c
 #include <unistd.h>
-
-void	ft_putchar(char c)
+	
+void    ft_putchar(char c)
 {
 		write(1, &c, 1);
 }
-
-void	ft_putstr(char *str)
-{
-		ft_putchar(*str);
-			str++;
+	
+void    ft_putstr(char *str)
+{ 
+	  	while (*str != '\0')
+	    	ft_putstr("Is the number positive or negative?"); 
+	      *str++;
 }
-
-void	ft_is_negative(int n)
-{
-	if (n >= 0)
-		ft_putstr("Positive");
-	else
-		ft_putstr("Negative");
+	
+void    ft_is_negative(int n)
+{  
+	    if (n >= 0)
+	        ft_putchar('P');
+    	else if (n < 0)
+			ft_putchar('N');
 }
-
-int main()
+int		main()
 {
-	ft_putstr("Is the number positive or negative?");
-	ft_is_negative();
-	return (0);
+		int n;
+
+		ft_is_negative(40);
+		ft_is_negative(-50);
+		ft_is_negative(234324);
+		ft_is_negative(-1);
+		ft_is_negative(-5);
+		return (0);
 }
 ```
-**Output:**
+**Output:** 
 ```c
-
+PNPNP
 ```
