8a674448469d5d03419833be4335322b9a4b72ed
Fred Siika <fredsiik@e1z1r4p21.42.us.org>
2019-01-18 00:38:51 -0800

    Changed parameter names

--- examshell/01-aff_first_param/aff_first_param.c
+++ examshell/01-aff_first_param/aff_first_param.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_first_param.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredsiik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 00:32:26 by fredsiik          #+#    #+#             */
+/*   Updated: 2019/01/18 00:34:39 by fredsiik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int				main(int ac, char **av)
+int	main(int argc_sucks, char **argv_sucks_even_more)
 {
-					if (ac > 1)
-							while (*av[1])
-									write(1, av[1]++, 1);
-					write(1, "\n", 1);
-					return(0);
+	if (argc_sucks > 1)
+		while (*argv_sucks_even_more[1])
+			write(1, argv_sucks_even_more[1]++, 1);
+	write(1, "\n", 1);
+	return(0);
 }
-
