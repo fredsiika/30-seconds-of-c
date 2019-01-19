4ae4ea0e29cb796edf329b6f95a542fc212f32d0
Fred Siika <fredsiika@gmail.com>
2019-01-18 16:54:56 -0800

    Print length of string.

---
+++ examshell/1-0-ft_strlen/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredsiik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 16:49:34 by fredsiik          #+#    #+#             */
+/*   Updated: 2019/01/18 16:54:33 by fredsiik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+// DO NOT SUBMIT ANYTHING BELOW
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[] = "Papito Frederico";
+
+	printf("%d\n", ft_strlen(str));
+	return (0);
+}
