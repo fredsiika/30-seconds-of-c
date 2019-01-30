cb1b36c9b4a9926102ce576130e168f0abe8de9c
Fred Siika <fredsiika@gmail.com>
2019-01-30 08:57:04 -0800

    Update ft_countdown.c

--- examshell/00-ft_countdown/ft_countdown.c
+++ examshell/00-ft_countdown/ft_countdown.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
 
-int				main(void)
+int	main(void)
 {
-					char c;
-					c = '9';
-					while (c >= '0')
-					{
-							write(1, &c, 1);
-							c--;
-					}
-					write(1, "\n", 1);
-					return (0);
+	char c;
+	c = '9';
+	while (c >= '0')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+	write(1, "\n", 1);
+	return (0);
 }
-
